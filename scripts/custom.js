f3a0cbfd93b616c17946a51a6296c0599f7ad48d
Aaron Jones <Amjcraft@gmail.com>
2015-10-15 11:11:56 -0500

    Responsive Tweaks
    Responsive Tweaks and scollTo bar

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,5 +1,19 @@
 
 /* Smooth scroll One page Nav-----------------------------------------------*/
-$(document).ready(function(){
-    $('nav a').ScrollTo();
+$(document).ready(function () {
+    $.extend($.scrollTo.defaults, {
+        duration: 600
+    });
+
+    $('#navigation li a').each(function (index, val) {
+        console.log(val);
+        $(this).on('click', function (event) {
+            event.preventDefault();
+            $(window).scrollTo($(val.hash));
+        })
+        
+    });
+
+
+    
 });
